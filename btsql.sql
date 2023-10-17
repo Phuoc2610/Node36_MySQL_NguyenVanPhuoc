0e7a31f9a30eeac6f0158367db18a95da59d289e
vanphuoc <phuoc0905886611@gmail.com>
2023-10-18 03:11:26 +0700

    push code

---
+++ btsql.sql
@@ -0,0 +1,121 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `food` (
+  `food_id` int NOT NULL AUTO_INCREMENT,
+  `type_id` int DEFAULT NULL,
+  `food_name` varchar(20) DEFAULT NULL,
+  `image` varchar(20) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `descr` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`food_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `food_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `food_type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `food_type` (
+  `type_id` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `like_res` (
+  `user_id` int DEFAULT NULL,
+  `res_id` int DEFAULT NULL,
+  `date_like` datetime DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `res_id` (`res_id`),
+  CONSTRAINT `like_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `like_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `orders` (
+  `user_id` int DEFAULT NULL,
+  `food_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `code` varchar(20) DEFAULT NULL,
+  `arr_sub_id` varchar(20) DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `food_id` (`food_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `rate_res` (
+  `user_id` int DEFAULT NULL,
+  `res_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `date_rate` datetime DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `res_id` (`res_id`),
+  CONSTRAINT `rate_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `rate_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `restaurant` (
+  `res_id` int NOT NULL AUTO_INCREMENT,
+  `res_name` varchar(20) DEFAULT NULL,
+  `image` varchar(20) DEFAULT NULL,
+  `descr` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`res_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `sub_food` (
+  `sub_id` int NOT NULL AUTO_INCREMENT,
+  `sub_name` varchar(20) DEFAULT NULL,
+  `sub_price` float DEFAULT NULL,
+  `food_id` int DEFAULT NULL,
+  PRIMARY KEY (`sub_id`),
+  KEY `food_id` (`food_id`),
+  CONSTRAINT `sub_food_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Select 
+-- 1.Tìm 5 người đã like nhà hàng nhiều nhất
+-- select U.full_name as TenKhachHang , count(*) as SoLuotLike
+-- from like_res LR join user U on LR.user_id = U.user_id
+-- GROUP by U.user_id
+-- order by COUNT(*) DESC
+-- limit 5;
+
+-- 2.Tìm 2 nhà hàng có lượt like nhiều nhất.
+-- select R.res_name as TenNhaHang , count(*) as SoLuotLike
+-- from like_res LR join restaurant R on LR.res_id = R.res_id
+-- GROUP by R.res_id
+-- order by COUNT(*) DESC
+-- limit 2;
+
+-- 3.Tìm người đã đặt hàng nhiều nhất.
+-- select U.full_name as TenKhachHang , count(*) as SoLanDatNhaHang
+-- from rate_res RR join user U on RR.user_id = U.user_id
+-- GROUP by U.user_id
+-- order by COUNT(*) DESC
+-- limit 1;
+
+-- 4. Tìm người dùng không hoạt động trong hệ thống
+-- select * from user 
+-- where user_id not in (select user_id from orders) 
+-- && user_id not in (select user_id from rate_res) 
+-- && user_id not in (select user_id from like_res)
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
